a6a87a0147a39ad59df338b656ebf0f8f9e6fa12
Jesus Cebader <zebader@hotmail.com>
2019-04-10 15:23:16 +0200

    screens and transitions

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
 'use strict';
-
 console.log('main');
+
+function main(){
+
+  const mainElement = document.querySelector('main');
+
+  function buildDom(html){
+    mainElement.innerHTML = html;
+    return mainElement;
+  }
+
+  function buildSplashScreen(){
+    const splashScreen = buildDom(`
+      <section>
+        <h1>Eternal Enemies</h1>
+        <button class="start-button">Start</button>
+      </section>
+    `)
+
+    const startButton = document.querySelector('.start-button');
+    startButton.addEventListener('click', buildGameScreen);
+
+
+
+  }
+  function buildGameScreen(){
+    const gameScreen = buildDom(`
+      <section class="game-container">
+        <canvas></canvas>
+      </section>
+    `)
+    const gameContainerElement = document.querySelector('.game-container');
+
+    const width = gameContainerElement.offsetWidth;
+    const height = gameContainerElement.offsetHeight;
+
+    const canvasElement = document.querySelector('canvas')
+
+    canvasElement.setAttribute('width',width);
+    canvasElement.setAttribute('height',height)
+
+    setTimeout(buildGameOverScreen,3000);
+    
+  }
+  function buildGameOverScreen(){
+    const gameOverScreen = buildDom(`
+      <section>
+        <h1>Game Over</h1>
+        <button class="restart-button">Restart</button>
+      </section>
+    `)
+
+    const restartButton = document.addEventListener('click',buildGameScreen)
+
+  }
+buildSplashScreen();
+}
+
+window.addEventListener('load',main); // espera a que cargue el DOM
